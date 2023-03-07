ccd60a96b798e9b5c4d96c3919aff913e8eb55a7
Francisco Vargas <P4nZh0p@gmail.com>
2023-03-06 21:22:05 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoJunto from './components/TodoJunto';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoJunto firstName="Jane" lastName="Doe" age={45} hairColor="Black"/>
+      <TodoJunto firstName="John" lastName="Smith" age={88} hairColor="Brown"/>
+      <TodoJunto firstName="Millard" lastName="Fillmore" age={50} hairColor="Brown"/>
+      <TodoJunto firstName="Maria" lastName="Smith" age={62} hairColor="Brown"/>
     </div>
-  );
+  )
 }
 
 export default App;
